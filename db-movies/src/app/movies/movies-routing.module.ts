394f2fb685266c170c18b6fbeec1b110d3530754
Tai Phan <tai101568@gmail.com>
2018-04-12 17:35:00 +0700

    add new component : "movie-detail" and move pagination to movies

--- db-movies/src/app/movies/movies-routing.module.ts
+++ db-movies/src/app/movies/movies-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { MoviesComponent } from './movies.component';
+import { MovieDetailComponent } from './movie-detail/movie-detail.component';
 const routes: Routes = [
   { path: '', component: MoviesComponent},
-  { path: ':category', component: MoviesComponent}
+  { path: ':category', component: MoviesComponent },
+  { path: ':category/detail/:id', component: MovieDetailComponent}
 ];
 
 @NgModule({
--- db-movies/src/app/movies/movies.module.ts
+++ db-movies/src/app/movies/movies.module.ts
@@ -4,13 +4,18 @@ import { CommonModule } from '@angular/common';
 import { MoviesRoutingModule } from './movies-routing.module';
 import { MoviesComponent } from './movies.component';
 import { DbMoivesService } from '../shared/db-moives.service';
+import { MovieDetailComponent } from './movie-detail/movie-detail.component';
+import { PaginationService } from '../theme/pagination-bar/pagination.service';
+import { NgbPagination } from '@ng-bootstrap/ng-bootstrap/pagination/pagination';
+import { ThemeModule } from '../theme/theme.module';
 
 @NgModule({
   imports: [
     CommonModule,
-    MoviesRoutingModule
+    MoviesRoutingModule,
+    ThemeModule
   ],
-  declarations: [MoviesComponent],
-  providers:[DbMoivesService]
+  declarations: [MoviesComponent, MovieDetailComponent],
+  providers:[DbMoivesService, PaginationService]
 })
 export class MoviesModule { }
